f15ad65eb65a204423bbd0bd47bcdcfec5dc71ff
Ankit Lal Shrestha <shresthaankitlal@gmail.com>
2025-07-16 16:47:07 +0545

    code changed in index.js

--- typescript/index.js
+++ typescript/index.js
@@ -2,9 +2,9 @@ const button = document.querySelector("button");
 const input1=document.getElementById("num1");
 const input2=document.getElementById("num2");
 
-const add=((num1,num2)=>{
-    return num1 + num2;
-})
+function sum(num1,num2){
+    return num1+num2;
+}
 button.addEventListener("click",function(){
-    console.log(add(input1.ariaValueMax,input2.value));
+    console.log(sum(input1.value,input2.value));
 })
