75a1a4634d80a1a301652a881237e73aab32f342
Ankit Lal Shrestha <shresthaankitlal@gmail.com>
2025-07-17 13:50:52 +0545

    download node js file

--- typescript/app.js
+++ typescript/app.js
@@ -1,1 +1,17 @@
-console.log("Your code goes here..");
+function sum(n1, n2, showResult, phrase) {
+    //   if(typeof n1 !=='number' || typeof n2 !=='number'){
+    //     throw new Error('incorrect input!');
+    //   }
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var number1 = 5;
+var number2 = 2.08;
+var printResult = true;
+var resultPhrase = 'result is:';
+sum(number1, number2, printResult, resultPhrase);
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,1 +1,17 @@
-console.log("Your code goes here..");
+function sum(n1:number,n2:number,showResult:boolean,phrase:string){
+//   if(typeof n1 !=='number' || typeof n2 !=='number'){
+//     throw new Error('incorrect input!');
+//   }
+const result=n1+n2;
+    if(showResult){
+        console.log(phrase+result)
+    }else{
+    return result;
+}}
+
+const number1=5;
+const number2=2.08;
+const printResult=true;
+const resultPhrase='result is:';
+
+sum(number1,number2,printResult,resultPhrase);
