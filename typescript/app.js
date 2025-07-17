53d97a118493eb109b4e7bf88278ccccfa7c4da4
Ankit Lal Shrestha <shresthaankitlal@gmail.com>
2025-07-17 16:09:41 +0545

    changing code in app.ts

--- typescript/app.js
+++ typescript/app.js
@@ -1,6 +1,17 @@
 var person = {
     name: 'John Cena',
     age: 30,
-    hobbies: ["sport", "playying"]
+    hobbies: ['cooking', 'playing'],
+    role: [2, 'author']
 };
-console.log(person.name, person.hobbies);
+person.hobbies.push('writing', 'sleeping');
+person.role.push('admin');
+person.role[1] = 10;
+person.role.push('book');
+var favouriteActivities;
+favouriteActivities = ['Sports'];
+console.log(person.name, person.hobbies, person.age, person.role);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,7 +1,18 @@
 const person={
     name:'John Cena',
     age:30,
-    hobbies:["sport","playying"]
+    hobbies:['cooking','playing'],
+    role:[2,'author']
 };
-console.log(person.name,person.hobbies)
+person.hobbies.push('writing','sleeping')
+person.role.push('admin');
+person.role[1]=10;
+person.role.push('book')
 
+let favouriteActivities:string[];
+favouriteActivities=['Sports'];
+console.log(person.name,person.hobbies,person.age,person.role)
+
+for(const hobby of person.hobbies){
+    console.log(hobby.toUpperCase());
+}
