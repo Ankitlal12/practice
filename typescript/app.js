b0e6e82c354cdfa484702a7f14005b8c41613b5f
Ankit Lal Shrestha <shresthaankitlal@gmail.com>
2025-07-18 11:27:18 +0545

    adding new infos

--- typescript/app.js
+++ typescript/app.js
@@ -6,7 +6,7 @@
 // console.log(combinedAges);
 // const combinedNames=combine('MAX','ANNA');
 // console.log(combinedNames)
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
     if (typeof input1 === 'number' && typeof input2 === 'number') {
         result = input1 + input2;
@@ -14,9 +14,16 @@ function combine(input1, input2) {
     else {
         result = input1.toString() + input2.toString();
     }
-    return result;
+    if (resultConversion === 'Hello  Kitty') {
+        return +result;
+    }
+    else {
+        return result.toString();
+    }
 }
-var combinedAges = combine(26, 30);
+var combinedAges = combine(26, 30, 'Hello kitty');
 console.log(combinedAges);
-var combinedNames = combine('MAX', 'ANNA');
+var combinedStringAges = combine('26', '30', 'Hell kitty');
+console.log(combinedStringAges);
+var combinedNames = combine('MAX', 'ANNA', 'hello kittu');
 console.log(combinedNames);
--- typescript/app.ts
+++ typescript/app.ts
@@ -9,18 +9,23 @@
 // const combinedNames=combine('MAX','ANNA');
 // console.log(combinedNames)
 
-function combine(input1:number|string,input2:number|string){
+function combine(input1:number|string,input2:number|string,resultConversion:string){
     let result;
 if(typeof input1==='number'&&typeof input2==='number'){
-
-
  result=input1+input2;}else{
     result=input1.toString() + input2.toString()
  }
-return result;
-  }
-const combinedAges=combine(26,30);
+ if(resultConversion==='Hello  Kitty'){
+return +result;}  
+  else{
+    return result.toString()
+  }}
+const combinedAges=combine(26,30,'Hello kitty');
 console.log(combinedAges);
 
-const combinedNames=combine('MAX','ANNA');
+const combinedStringAges=combine('26','30','Hell kitty');
+console.log(combinedStringAges)
+
+
+const combinedNames=combine('MAX','ANNA','hello kittu');
 console.log(combinedNames)
